25bc6d9b3f0816b753e0042927e8c096a33a30d1
David Cruciani <david.cruciani@circl.lu>
2024-04-05 11:23:25 +0200

    new: [bot] basic command

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # matrix-gif-sender
+
 Bot that send random gif
+
+
+
+## Installation
+
+```bash
+sudo apt-get update -y
+sudo apt-get install libolm-dev -y
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## Tenor
+
+It's required an api key and a client key that can be obtain on [tenor website](https://developers.google.com/tenor/guides/quickstart)
+
+
+
+## Usage
+
+First, make a copy of `config.sample.py` to `config.py` and fill it.
+
+Then run:
+
+```bash
+python main.py
+```
+
+Go to a room and enter a message like this:
+
+```
+!gif excited
+```
---
+++ main.py
@@ -0,0 +1,53 @@
+"""
+Example Usage:
+
+random_user
+      !gif excited
+"""
+
+import simplematrixbotlib as botlib
+import config as Config
+import requests
+import random
+import json
+
+config = botlib.Config()
+config.encryption_enabled = True  # Automatically enabled by installing encryption support
+config.ignore_unverified_devices = True
+
+creds = botlib.Creds(Config.MATRIX_SERVER, Config.MATRIX_USER, Config.MATRIX_PASSWORD, device_name=Config.MATRIX_DEVICE_NAME)
+bot = botlib.Bot(creds, config)
+PREFIX = '!'
+
+image_filepath = "gif_sender.gif"
+limit=200
+
+def get_gif(searchTerm):
+    if Config.TENOR_API_KEY == "LIVDSRZULELA":
+        response = requests.get(f"https://g.tenor.com/v1/random?q={searchTerm}&key={Config.TENOR_API_KEY}&limit=1")
+        data = response.json()
+        
+        uri = data['results'][0]['media'][0]['gif']['url']
+    else:
+        response = requests.get(f"https://tenor.googleapis.com/v2/search?q={searchTerm}&key={Config.TENOR_API_KEY}&client_key={Config.TENOR_CLIENT_KEY}&limit={limit}")
+        data = json.loads(response.content)
+        
+        # see urls for all GIFs
+        gif = random.choice(data['results'])
+        uri = gif["media_formats"]["gif"]["url"]
+
+    with open(image_filepath, "wb") as f:
+        f.write(requests.get(uri).content)
+    
+
+@bot.listener.on_message_event
+async def gif_sender(room, message):
+    global last_command
+    match = botlib.MessageMatch(room, message, bot, PREFIX)
+
+    if match.prefix():
+        if match.command("gif"):
+            get_gif(" ".join(arg for arg in match.args()))
+            await bot.api.send_image_message(room.room_id, image_filepath)
+
+bot.run()
